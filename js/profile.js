d7b757c38de7ce5979df33e8b27029fb15782143
unknown <thegirlwholovesthesky@gmail.com>
2019-11-28 23:05:42 +0800

    attempt fix profile

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,7 @@
 
 
 function show_profile(){
+    alert("called")
     let prof_parent = document.getElementsByClassName("prof-container")[0];
     prof_parent.innerHTML = 
     "<br><h2>Name:</h5> " + prof_info[0].name +
