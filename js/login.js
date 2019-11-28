443a99352a55bde64e68f026befe1f03c3502cb6
unknown <thegirlwholovesthesky@gmail.com>
2019-11-28 22:06:34 +0800

    deleted some test

--- js/login.js
+++ js/login.js
@@ -23,8 +23,7 @@
                 if (userType == "Student") {
                   window.location.replace("./html/home-student.html");
                 } else {
-                  console.log("WENT TO LOG IN")
-                  window.location.replace("./html/home-teacher.html?user=" + authResult.user.uid.toString());
+                  window.location.replace("./html/home-teacher.html?");
                 }
                 flag = 1;
               }
--- js/register.js
+++ js/register.js
@@ -21,10 +21,9 @@ firebase.auth().onAuthStateChanged(function(user) {
           if (universityID != "") {
             var userType = childsnapshot.child("UserType").val();
             if (userType == "Student") {
-              // window.location.replace("../html/home-student.html" + "?/user=" + user.key);
-              location.href = "../html/home-student.html";
+              window.location.replace("../html/home-student.html");
             } else {
-              window.location.replace("../html/home-teacher.html?/user=" + firebase.auth().currentUser.uid);
+              window.location.replace("../html/home-teacher.html");
             }
             flag = 1;
           }
