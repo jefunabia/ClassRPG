ba16db48ffa25a5fabb86a043061055a9f021086
unknown <thegirlwholovesthesky@gmail.com>
2019-11-28 21:33:49 +0800

    test jm #!2

--- js/login.js
+++ js/login.js
@@ -23,7 +23,14 @@
                 if (userType == "Student") {
                   window.location.replace("./html/home-student.html");
                 } else {
-                  window.location.replace("./html/home-teacher.html");
+                  // window.location.replace("./html/home-teacher.html");
+                  firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+                    // Send token to your backend via HTTPS
+                    console.log(idToken)
+                    window.location.replace("./html/home-teacher.html" +  "?/user=" + idToken.key);
+                  }).catch(function(error) {
+                    // Handle error
+                  });
                 }
                 flag = 1;
               }
